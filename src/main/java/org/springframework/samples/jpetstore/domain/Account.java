f99fa95a30419e5728346e730390ca8db002c7dc
Jon <52193917+juananmora@users.noreply.github.com>
2020-06-03 08:09:33 +0200

    Update Account.java

--- src/main/java/org/springframework/samples/jpetstore/domain/Account.java
+++ src/main/java/org/springframework/samples/jpetstore/domain/Account.java
@@ -8,7 +8,7 @@ public class Account implements Serializable {
   /* Private Fields */
 
   private String username;
-  private String password;
+  public String password;
   private String email;
   private String firstName;
   private String lastName;
